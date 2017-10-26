fd9af0ed8cf46fbec9095861f324e594adf1c9d1
Fabio Ticconi <fabio.ticconi@gmail.com>
2017-10-26 08:10:51 +0200

    upping version, 0.1.0 is fair enough now; also improving script to make package

--- make_pkg.sh
+++ make_pkg.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
+v="0.1.0"
+
 mvn clean package -Dmaven.test.skip=true
 
-cp target/alone-0.0.1-SNAPSHOT-jar-with-dependencies.jar alone-0.0.1.jar
+cp target/alone-$v-SNAPSHOT-jar-with-dependencies.jar alone-$v.jar
 
-7z a -tzip alone.zip data/ alone-0.0.1.jar
+7z a -tzip alone-$v.zip data/ alone-$v.jar
 
-rm alone-0.0.1.jar
+rm alone-$v.jar
