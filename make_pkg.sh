08f0162c1cf7be2cd6e3575380d2e07416058fa8
Fabio Ticconi <fabio.ticconi@gmail.com>
2017-10-26 08:32:50 +0200

    some more renaming

--- make_pkg.sh
+++ make_pkg.sh
@@ -4,8 +4,8 @@ v="0.1.0"
 
 mvn clean package -Dmaven.test.skip=true
 
-cp target/alone-$v-SNAPSHOT-jar-with-dependencies.jar alone-$v.jar
+cp target/alone-rl-$v-SNAPSHOT-jar-with-dependencies.jar alone-rl-$v.jar
 
-7z a -tzip alone-$v.zip data/ alone-$v.jar
+7z a -tzip alone-rl-$v.zip data/ alone-rl-$v.jar
 
-rm alone-$v.jar
+rm alone-rl-$v.jar
