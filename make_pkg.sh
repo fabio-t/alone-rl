bc7b42e1d97c4dfea0bc8472b375408b70c1e84b
Fabio Ticconi <fabio.ticconi@gmail.com>
2017-11-04 23:14:39 +0100

    fixing script

--- make_pkg.sh
+++ make_pkg.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-v="0.1.0"
+v=`ls target/alone-rl-*-jar-with-dependencies.jar | awk -F"-" '{print $3}'`
 
 mvn clean package -Dmaven.test.skip=true
 
-cp target/alone-rl-$v-SNAPSHOT-jar-with-dependencies.jar alone-rl-$v.jar
+cp target/alone-rl-*-jar-with-dependencies.jar alone-rl-$v.jar
 
 7z a -tzip alone-rl-$v.zip data/ alone-rl-$v.jar
 
